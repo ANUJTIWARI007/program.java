af101286349e88aeb45f74536bf463478d812130
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-12 16:51:35 +0530

    Create program7.java

---
+++ java/program7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ public class Main{
+  public static void main(String[]args){
+    float km , mile;
+    java.util.Scanner sc = new Scanner(System.in);
+
+    System.out.println("enter km");
+    km = sc.nextFloat();
+    
+  
+    mile = km/1.6f;
+    System.out.println(mile);
+
+    
+   
+   
+   
+   
+   }
+
+}
+
