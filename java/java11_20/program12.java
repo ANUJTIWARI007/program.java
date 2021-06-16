ccf9c4900b16216a3f0d1efaecf724b5e24b419e
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-16 14:55:52 +0530

    Create program12.java

---
+++ java/java11_20/program12.java
@@ -0,0 +1,14 @@
+class Main{
+  public static void main(String[]args){
+    int i=1,s=0,f=1;
+    for(i=1;i<=10;i++){
+    s=s+i;
+    if(i<=5){
+    f=f*i;
+    }
+    }
+   System.out.println(s);
+   System.out.println(f);
+  }
+
+}
