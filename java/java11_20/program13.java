cff87763a97354b0c02c485e0331852dc7152fff
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-17 23:27:33 +0530

    Create program13.java

---
+++ java/java11_20/program13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[]args){
+  int a=98;
+  Scanner sc = new Scanner(System.in);
+  int b = sc.nextInt();
+  System.out.println(a<b);
+  
+
+  }
+
+}
+
+
