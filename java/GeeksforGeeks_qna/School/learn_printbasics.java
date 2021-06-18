f1feb53e8c9bd7b15a432a9e5f8e9a199b63631e
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-18 23:36:02 +0530

    Create learn_printbasics.java

---
+++ java/GeeksforGeeks_qna/School/learn_printbasics.java
@@ -0,0 +1,11 @@
+
+//User function Template for Java
+class Solution{
+    static void printIntString(String S,int N){
+        S = "GeeksforGeeks";  //asign values
+        N = 20;
+        //print values
+        System.out.println("the input string:"+S);
+        System.out.println(" the input  integer:"+N);
+    }
+}
