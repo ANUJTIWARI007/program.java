ba0711c2db020abe37916d9bad3ad6e7b2793127
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-19 16:11:01 +0530

    Create program16replace.java

---
+++ java/java11_20/program16replace.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[]args){
+    String name = "a n u j";
+    String trim = name.replace(' ','_');
+    System.out.println(trim);
+    
+  
+
+  }
+
+}
+
+
