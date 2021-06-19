fff42b9ea181193963ef445679650c1448f3bee2
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-19 17:10:17 +0530

    Create ifElseprogram.java

---
+++ java/GeeksforGeeks_qna/School/ifElseprogram.java
@@ -0,0 +1,17 @@
+class Solution{
+    static String compareNM(int n,int m){
+        if(m>n)
+        {
+            return "lesser";
+        }
+        else
+        {
+            if(n==m){
+              return "equal";}
+            else
+            {
+            return "greater";}
+        }
+        
+    }
+}
