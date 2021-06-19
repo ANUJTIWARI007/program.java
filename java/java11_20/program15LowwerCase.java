0e1ed949e9b944a31f3cbe63e7c744d2cc334686
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-19 16:01:00 +0530

    Create program15LowwerCase.java

---
+++ java/java11_20/program15LowwerCase.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[]args){
+    String name = "Harry";
+    String Lstring = name.toLowerCase();
+    System.out.println(Lstring);
+  
+
+  }
+
+}
+
+
