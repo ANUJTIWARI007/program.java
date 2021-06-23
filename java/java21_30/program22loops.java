d26e1f7043ae05be151bc66a4effd1fb614414f6
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-23 16:54:10 +0530

    Create program22loops.java

---
+++ java/java21_30/program22loops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Main{
+  public static void main(String[]args){
+    System.out.println("enter your name");
+   Scanner sc = new Scanner(System.in);
+   String s= sc.next();
+    int i; 
+
+    for(i=1;i<=5;i++)
+    {
+      System.out.println(s);
+    }
+  }
+}
+
+
