4db94f83a40cd991506e33a348698339729b73a0
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-06-29 15:07:13 +0530

    Create program_40.java

---
+++ java/java31_40/program_40.java
@@ -0,0 +1,28 @@
+import java.uitl.Scanner;
+
+public class test{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+
+		if (n == 1)
+		{
+			System.out.println("Neither prime nor composite");
+			return;
+		}
+
+		for (int i = 2; i * i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				System.out.println("Composite");
+				return;
+			}
+		}
+
+		System.out.println("Prime");
+	}
+}
