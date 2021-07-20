a5311b0c8433be1edce65d82a390b1ad8b184e13
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-07-21 00:07:26 +0530

    Create programRightAngle.java

---
+++ java/starpattern/programRightAngle.java
@@ -0,0 +1,16 @@
+public class Main{
+   public static void main(String[]args){
+      int j,i;
+      for(i = 1; i <= 5; i++)
+    {
+      for(j = 1; j <= 5; j++)
+      {
+         if(j <= i)
+         System.out.print("*");
+         else
+            System.out.print(" ");
+      }
+      System.out.print("\n");
+    }
+   }
+}
