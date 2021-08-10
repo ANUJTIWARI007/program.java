ef7b35fe00d7f72e45f09386f167ea966e051a22
ANUJTIWARI007 <85436239+ANUJTIWARI007@users.noreply.github.com>
2021-08-10 16:35:44 +0530

    Create program17.java

---
+++ java/starpattern/program17.java
@@ -0,0 +1,17 @@
+public class Main{
+  public static void main(String[]args)
+  {
+    int i , j;
+    for(i = 1; i <= 5; i++)
+    {
+      for(j = 1; j <= 9; j++)
+        {
+          if(j >= i && j <= 10 - i)
+            System.out.print("*");
+          else
+            System.out.print(" ");
+        }
+       System.out.print("\n");
+    }   
+  }
+}
